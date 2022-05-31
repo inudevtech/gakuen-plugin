2cb30944c0ea16a7db67016b49c02c0588be7694
Riku Ueda <riku.ueda@outlook.jp>
2022-05-31 20:02:43 +0900

    プラグインの実装

---
+++ src/main/java/tech/inudev/gakuenplugin/GakuenCommand.java
@@ -0,0 +1,31 @@
+package tech.inudev.gakuenplugin;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+public class GakuenCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("photograph")) { //親コマンドの判定
+            if (args.length == 0) { //子コマンドがない場合
+                sender.sendMessage("/photograph [on|off]");
+            } else if (args.length == 1) { //子コマンドがある場合
+                if (args[0].equalsIgnoreCase("on")) { //子コマンドがonの場合
+                    GakuenPlugin.setPhotograph(true);
+                    // TODO: コマンドの部分をクリックしたら実行できるように。
+                    sender.sendMessage("撮影モードが有効になりました。\n全員が退出するか、§9§n/photograph off§rを実行すると撮影モードが無効になります。\nこのメッセージはF3+Dを押すことで消すことができます。");
+                    return true;
+                } else if (args[0].equalsIgnoreCase("off")) { //子コマンドがoffの場合
+                    GakuenPlugin.setPhotograph(false);
+                    sender.sendMessage("撮影モードが無効になりました。");
+                    return true;
+                } else { //子コマンドがその他の場合
+                    sender.sendMessage("/photograph [on|off]");
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/tech/inudev/gakuenplugin/GakuenPlugin.java
@@ -0,0 +1,59 @@
+package tech.inudev.gakuenplugin;
+
+import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class GakuenPlugin extends JavaPlugin implements Listener {
+
+    @Getter private static boolean photograph;
+    public static void setPhotograph(boolean photograph) {
+        GakuenPlugin.photograph = photograph;
+        // TODO: BossBar化
+        Bukkit.getOnlinePlayers().forEach(p -> {
+            if(!p.hasPermission("gakuenplugin.gorakuba"))
+                p.sendTitle("§c撮影中です！", "§cご注意を。", 0, 80, 10);
+        });
+    }
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        if (photograph) {
+            event.setJoinMessage(null);
+            event.getPlayer().sendTitle("§c撮影中です！", "§cご注意を。", 0, 80, 10);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        if(photograph && !event.getPlayer().hasPermission("gakuenplugin.approvallogin")) {
+            event.disallow(PlayerLoginEvent.Result.KICK_OTHER, "只今撮影中です！しばらくしてから再接続して下さい！");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (photograph) {
+            event.setQuitMessage(null);
+        }
+        if(getServer().getOnlinePlayers().stream().noneMatch(player -> player.hasPermission("gakuenplugin.gorakuba"))){
+            photograph = false;
+        }
+    }
+}
