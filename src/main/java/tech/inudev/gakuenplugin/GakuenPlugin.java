721b1465a98f47e84c844c4bb06175e7e2b90027
Riku Ueda <riku.ueda@outlook.jp>
2022-06-11 22:00:35 +0900

    バグ修正

--- src/main/java/tech/inudev/gakuenplugin/GakuenPlugin.java
+++ src/main/java/tech/inudev/gakuenplugin/GakuenPlugin.java
@@ -87,10 +87,11 @@ public final class GakuenPlugin extends JavaPlugin implements Listener {
     public void onPlayerQuit(PlayerQuitEvent event) {
         if (photograph) {
             event.setQuitMessage(null);
-        }
-        if (getServer().getOnlinePlayers().stream().noneMatch(player -> player.hasPermission("gakuenplugin.gorakuba"))) {
-            getServer().broadcastMessage("撮影者がいなくなったため、撮影モードを無効化しました。");
-            setPhotograph(false);
+
+            if (getServer().getOnlinePlayers().stream().noneMatch(player -> player.hasPermission("gakuenplugin.gorakuba"))) {
+                getServer().broadcastMessage("撮影者がいなくなったため、撮影モードを無効化しました。");
+                setPhotograph(false);
+            }
         }
     }
 }
